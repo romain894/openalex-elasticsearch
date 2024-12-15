bfe1aca8a4d5f739fbaa3e72b56b02d8fea01796
Romain THOMAS <38260500+romain894@users.noreply.github.com>
2024-12-15 20:16:19 +0100

    added API: implemented vector search endpoint

---
+++ api.py
@@ -0,0 +1,50 @@
+import config
+from config import client, inference_chunk_size
+from log_config import log
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from ml import encode_text_document
+
+
+class Embedding(BaseModel):
+    vector: list[float]
+
+
+app = FastAPI()
+
+
+@app.post("/vector_knn_search")
+def vector_knn_search(embedding: Embedding):
+    """
+    Retrieve the nearest neighbors of an embedding with a KNN search (fast but approximate).
+    :param embedding: The embedding for the search.
+    :return: The nearest embeddings.
+    """
+    resp = client.search(
+        index="works",
+        knn={
+            "field": "abstract_embeddings",
+            "query_vector": embedding.vector,
+            "k": 10,
+            "num_candidates": 5000
+        }
+    )
+    res = [{
+        "id": w["_source"]["id"],
+        "display_name": w["_source"]["display_name"],
+        "abstract": w["_source"]["abstract"],
+    } for w in resp["hits"]["hits"]]
+    return res
+
+
+@app.post("/text_knn_search")
+def text_knn_search(text: str):
+    """
+    Retrieve the similar abstracts with a KNN search (fast but approximate).
+    :param text: The text for the search.
+    :return: The similar abstracts.
+    """
+    text_embedding = Embedding(vector=encode_text_document(text))
+    res = vector_knn_search(text_embedding)
+    return res
